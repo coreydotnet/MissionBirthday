20c1576306d234050969d102fcb69ac51fba34bb
Bradley Carestia <bradley.dotnet@gmail.com>
2022-03-27 13:39:48 -0700

    update UI

--- MissionBirthday.UI/projects/mb-mobile/src/app/pages/search-results/search-results-page.component.ts
+++ MissionBirthday.UI/projects/mb-mobile/src/app/pages/search-results/search-results-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Observable } from 'rxjs';
+import { catchError, Observable, throwError } from 'rxjs';
 import { CharityEvent } from '../../interfaces/charity-event.interface';
 import { EventService } from '../../services/event.service';
 
@@ -10,13 +10,19 @@ import { EventService } from '../../services/event.service';
   styleUrls: ['./search-results-page.component.scss']
 })
 export class SearchResultsPageComponent implements OnInit {
-  public event$: Observable<CharityEvent[]>
+  public event$: Observable<CharityEvent[]>;
+  public error: string;
 
   constructor(private readonly route: ActivatedRoute,
     private readonly events: EventService) { }
 
   ngOnInit(): void {
-    this.event$ = this.events.getAreaEvents(this.route.snapshot.params.zipCode);
+    this.event$ = this.events.getAreaEvents(this.route.snapshot.params.zipCode).pipe(
+      catchError((err) => {
+        this.error = 'Invalid Zip Code';
+        return throwError(err);
+      })
+    );
   }
 
 }
